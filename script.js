003f744e1b289d3b8ece1aafb752420b167bfa4e
abhinv <abhinavroy2099@gmail.com>
2023-08-10 21:18:55 +0530

    complete the js file

--- script.js
+++ script.js
@@ -1,46 +1,64 @@
 const buttons = document.querySelectorAll(".button-box");
 const display = document.querySelector(".inner-div1");
 console.log("helo");
-var operand1 = 0;
-var operand2 = null;
-var operator = null;
+var operand1 = "";
+var operand2 = "";
+var operator = "";
+var fullExpression = "";
+
+function updateDisplay() {
+  fullExpression = operand1 + " " + operator + " " + operand2;
+  display.textContent = fullExpression;
+}
+
 function isOperator(value) {
   return value == "+" || value == "-" || value == "*" || value == "/";
 }
 
-var inputSequence = ""; // Variable to store the entire input sequence
-
 for (var i = 0; i < buttons.length; i++) {
   buttons[i].addEventListener("click", function () {
     value = this.getAttribute("data-value");
-    var text = display.textContent.trim();
 
     if (isOperator(value)) {
-      if (operand1 === null) {
-        operand1 = parseFloat(inputSequence);
+        console.log(value);
+      if (operator === "") {
         operator = value;
-        inputSequence += " " + value + " "; // Update the input sequence
-        display.textContent = inputSequence; // Update the display
+        updateDisplay();
       }
-    } else if (value == "del") {
-      inputSequence = inputSequence.slice(0, -1); // Remove last character from the sequence
-      display.textContent = inputSequence; // Update the display
+    } else if (value === "del") {
+      // Handle the DEL functionality
+      if (operator === "") {
+        operand1 = operand1.slice(0, -1);
+      } else {
+        operand2 = operand2.slice(0, -1);
+      }
+      updateDisplay();
+    } else if (value == "ac") {
+      operand1 = "";
+      operand2 = "";
+      operator = "";
+      fullExpression = "";
+      display.textContent = "";
     } else if (value == "=") {
-      if (operand1 !== null && operator !== null) {
-        operand2 = parseFloat(inputSequence);
-        var result = eval(operand1 + " " + operator + " " + operand2);
-        console.log(result);
-        if (!isNaN(result)) {
-          inputSequence = result.toString(); // Store the result in the sequence
-          display.textContent = inputSequence; // Update the display
-          operand1 = result;
-          operand2 = null;
-          operator = null;
-        }
+      operand2 = fullExpression.slice(operand1.length + operator.length + 2);
+      var result = eval(operand1 + " " + operator + " " + operand2);
+      console.log(result);
+      if (!isNaN(result)) {
+        operand1 = result.toString();
+        operand2 = "";
+        operator = "";
+        fullExpression = operand1;
+        display.textContent = result;
       }
     } else {
-      inputSequence += value; // Update the input sequence
-      display.textContent = inputSequence; // Update the display
+      if (operator === "") {
+          operand1 += value;
+          console.log(value);
+        } else {
+            operand2 += value;
+            console.log(value);
+      }
+      updateDisplay();
     }
   });
 }
